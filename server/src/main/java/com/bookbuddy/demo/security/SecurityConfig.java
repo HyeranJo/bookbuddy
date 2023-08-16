06959a91532725fc35d357da636b5512906612ff
johyeran <a01026823164@gmail.com>
2023-08-16 15:27:43 +0900

    Feat: 의존성 라이브러리 추가 및 security 환경에서 h2 사용가능하도록 수정

---
+++ server/src/main/java/com/bookbuddy/demo/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.bookbuddy.demo.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().ignoringAntMatchers("/h2/**")
+                .and()
+                .headers().frameOptions().sameOrigin();
+    }
+}
