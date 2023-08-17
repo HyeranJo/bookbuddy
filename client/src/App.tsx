95d9b0f67dd35fdb29449cbe378d173a3bcce472
AngryDoggaebi <aofhoa@gmail.com>
2023-08-18 01:17:42 +0900

    Feat: info nav 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/nav/Nav';
 import Header from './components/header/Header';
+import InfoNav from './components/info_nav/InfoNav';
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <InfoNav />
     </div>
   );
 }
---
+++ client/src/components/info_nav/InfoNav.style.ts
@@ -0,0 +1,48 @@
+import { styled } from 'styled-components';
+
+export const Styled_InfoNav = {
+  Div: styled.div`
+    width: 150px;
+    height: 188px;
+    border: 1px solid var(--primary-background-color);
+
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+
+    font-size: var(--basic-font-size);
+    gap: 20px;
+
+    position: relative;
+
+    &::after {
+      border-color: white transparent;
+      border-style: solid;
+      border-width: 0 12px 17px 12px;
+      content: '';
+      display: block;
+      left: 60px;
+      position: absolute;
+      top: -15px;
+      width: 0;
+      z-index: 1;
+    }
+
+    &::before {
+      border-color: var(--primary-background-color) transparent;
+      border-style: solid;
+      border-width: 0 12px 17px 12px;
+      content: '';
+      display: block;
+      left: 60px;
+      position: absolute;
+      top: -17px;
+      width: 0;
+      z-index: 0;
+    }
+  `,
+  Span: styled.span`
+    cursor: pointer;
+  `,
+};
