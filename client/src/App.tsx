58149eb46f411eb9c261cbfa2709459ac075d88a
Yeondong Choe <125066377+YeondongChoe@users.noreply.github.com>
2023-08-18 12:05:34 +0900

    Feat fe/book (#6)
    * feat: book 컴포넌트 구현
    
    * merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from './components/nav/Nav';
 import Header from './components/header/Header';
 import InfoNav from './components/info_nav/InfoNav';
+import Book from './components/book/Book';
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <Book name="나의 라임 오렌지나무" />
       <InfoNav />
     </div>
   );
