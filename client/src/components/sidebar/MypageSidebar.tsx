615f7d2cd7295ac18bd25b9991a0855c66a0e467
AngryDoggaebi <aofhoa@gmail.com>
2023-08-22 01:33:40 +0900

    Feat: mypage 사이드바 클릭시 스크롤 이동 구현

--- client/src/components/sidebar/MypageSidebar.tsx
+++ client/src/components/sidebar/MypageSidebar.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
 import { Styled_MypageSidebar } from './MypageSidebar.style';
+import { useSetRecoilState } from 'recoil';
+import { NavScrollAtom } from '../../recoil/NavScroll';
 
 const MypageSidebar = () => {
+  const setNavScroll = useSetRecoilState(NavScrollAtom);
+
+  const handleScrollView = (e: any) => {
+    const menu = e.target.innerText;
+    const category: { [key: string]: number } = {
+      마이페이지: 0,
+      '전체 주문 내역': 1,
+      '1:1 문의 내역': 2,
+      '북마크 리스트': 3,
+    };
+
+    setNavScroll(category[menu]);
+  };
+
   return (
     <Styled_MypageSidebar.Container>
       <Styled_MypageSidebar.Div>
         <Styled_MypageSidebar.TitleDiv>
-          <span>마이페이지</span>
+          <span onClick={handleScrollView}>마이페이지</span>
         </Styled_MypageSidebar.TitleDiv>
         <Styled_MypageSidebar.CategoryDiv>
-          <span>전체 주문 내역</span>
-          <span>1:1 문의 내역</span>
-          <span>북마크 리스트</span>
+          <span onClick={handleScrollView}>전체 주문 내역</span>
+          <span onClick={handleScrollView}>1:1 문의 내역</span>
+          <span onClick={handleScrollView}>북마크 리스트</span>
         </Styled_MypageSidebar.CategoryDiv>
       </Styled_MypageSidebar.Div>
     </Styled_MypageSidebar.Container>
--- client/src/pages/mypage/Mypage.tsx
+++ client/src/pages/mypage/Mypage.tsx
@@ -4,34 +4,78 @@ import { Styled_Layout } from '../BlankPageLayout';
 import { Styled_Mypage } from './Mypage.style';
 import MypageTable from '../../components/category/MypageTable';
 import Book from '../../components/book/Book';
+import { useRecoilValue } from 'recoil';
+import { NavScrollAtom } from '../../recoil/NavScroll';
 
 const Mypage = () => {
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const bookmarkScrollRef = useRef<HTMLDivElement>(null);
+  const navScrollListRef = useRef<any>([]);
+  const navScrollIndex = useRecoilValue(NavScrollAtom);
+
   useEffect(() => {
+    // bookmark scroll
     // 현재 스크롤 위치 === scrollRef.current.scrollTop
     // 스크롤 길이 === scrollRef.current.scrollHeight
-    if (scrollRef.current)
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    if (bookmarkScrollRef.current) {
+      bookmarkScrollRef.current.scrollTop =
+        bookmarkScrollRef.current.scrollHeight;
+    }
   });
+
+  useEffect(() => {
+    // sidebar scroll
+    if (navScrollListRef.current) {
+      navScrollListRef.current[navScrollIndex].scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+
   return (
     <Styled_Layout.Container>
       <Styled_Layout.Div>
         <MypageSidebar />
         <Styled_Mypage.Content>
           <Styled_Mypage.Title>
-            <Styled_Mypage.H1>000님, 환영합니다</Styled_Mypage.H1>
+            <div
+              style={{ scrollMarginTop: '240px' }}
+              ref={el => {
+                navScrollListRef.current[0] = el;
+              }}
+            >
+              <Styled_Mypage.H1>000님, 환영합니다</Styled_Mypage.H1>
+            </div>
             <Styled_Mypage.Point>포인트 1,000P</Styled_Mypage.Point>
           </Styled_Mypage.Title>
 
           <Styled_Mypage.Detail className="detail">
             <MypageTable title="배송 정보" cancel={true} />
-            <MypageTable title="전체 주문 내역" />
-            <MypageTable title="1:1 문의 내역" />
+            <div
+              style={{ scrollMarginTop: '240px' }}
+              ref={el => {
+                navScrollListRef.current[1] = el;
+              }}
+            >
+              <MypageTable title="전체 주문 내역" />
+            </div>
+            <div
+              style={{ scrollMarginTop: '240px' }}
+              ref={el => {
+                navScrollListRef.current[2] = el;
+              }}
+            >
+              <MypageTable title="1:1 문의 내역" />
+            </div>
 
-            <Styled_Mypage.BookmarkTitle>
+            <Styled_Mypage.BookmarkTitle
+              style={{ scrollMarginTop: '240px' }}
+              ref={el => {
+                navScrollListRef.current[3] = el;
+              }}
+            >
               <Styled_Mypage.H1>북마크 리스트</Styled_Mypage.H1>
             </Styled_Mypage.BookmarkTitle>
-            <Styled_Mypage.Books ref={scrollRef} className="books">
+            <Styled_Mypage.Books ref={bookmarkScrollRef} className="books">
               <Styled_Mypage.Book>
                 <Book />
               </Styled_Mypage.Book>
