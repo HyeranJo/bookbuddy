093ab1a4ec24caa597c75131541813ba6d7de181
AngryDoggaebi <aofhoa@gmail.com>
2023-08-27 23:07:16 +0900

    Fix: useEffect에서 무한히 리렌더링 되는 문제 해결

--- client/src/components/category/CartTable.tsx
+++ client/src/components/category/CartTable.tsx
@@ -20,32 +20,32 @@ const CartTable = () => {
   const [totalPrice, settotalPrice] = useState(0);
 
   useEffect(() => {
-    const getOrderList = async () => {
-      try {
-        const response = await axios.get('./dummy/orderDummy.json');
-        setOrderList(response.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getOrderList();
+    if (orderList.length === 0) {
+      const getOrderList = async () => {
+        try {
+          const response = await axios.get('./dummy/orderDummy.json');
+          setOrderList(response.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getOrderList();
+    }
+  }, []);
 
-    // 0 or 1 체크리스트 배열 만들기
-    if (orderList.length !== 0) {
+  useEffect(() => {
+    // 처음 한 번만 0 or 1 체크리스트 배열 만들기
+    if (checkList.length === 0) {
       const arr = Array(orderList.length)
         .fill(1)
         .map(v => v);
 
       setCheckList(arr);
     }
-  }, [orderList]);
 
-  // 새로고침 하지 않는 이상 checklist만 바뀔 것이기 때문에 데이터 다시 받아오면 안 됨.
-  // useEffect 분리
-  useEffect(() => {
-    const arr = [];
     // 체크리스트에 1인 아이들만 orderlist에서 찾아서 수량*금액 다 더해서 총합에 리턴
-    for (let i = 0; i < orderList.length; i++) {
+    const arr = [];
+    for (let i = 0; i < checkList.length; i++) {
       if (checkList[i] === 1) {
         arr.push(orderList[i].quantity * orderList[i].price);
       }
@@ -58,6 +58,15 @@ const CartTable = () => {
       );
     }
   }, [checkList]);
+
+  // checkbox 수정하는 함수
+  const checkListHandler = (index: number) => {
+    const copy = [...checkList];
+    copy[index] = 0;
+    console.log(copy);
+    /** @todo: checkbox check 푸는 로직  */
+    setCheckList(copy);
+  };
 
   return (
     <>
@@ -88,7 +97,13 @@ const CartTable = () => {
                 <React.Fragment key={v.id}>
                   <Styled_CartTable.Tr>
                     <td rowSpan={2}>
-                      <Styled_CartTable.Input type="checkbox" />
+                      <Styled_CartTable.Input
+                        type="checkbox"
+                        onChange={() => {
+                          checkListHandler(i);
+                        }}
+                        checked
+                      />
                     </td>
                     <td rowSpan={2}>
                       <Styled_CartTable.Img src={v.imgSrc} />
@@ -116,7 +131,7 @@ const CartTable = () => {
             <td colSpan={5}>배송비 3,000원</td>
           </Styled_CartTable.AmountTr>
           <Styled_CartTable.AmountTr className="total">
-            <td colSpan={5}>합계 {totalPrice}원</td>
+            <td colSpan={5}>합계 {totalPrice + 3000}원</td>
           </Styled_CartTable.AmountTr>
         </tbody>
       </Styled_CartTable.Table>
