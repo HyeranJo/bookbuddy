2856182e83c432029d5fff915a1b83193b7caf31
AngryDoggaebi <aofhoa@gmail.com>
2023-09-01 18:52:07 +0900

    Feat: 카트 아이템 수량 변경시 서버로 보내는 로직 구현

--- client/src/components/quantity/QuantityInput.tsx
+++ client/src/components/quantity/QuantityInput.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Styled_QuantityInput } from './QuantityInput.style';
-import { useRecoilState } from 'recoil';
-import { QuantityListAtom } from '../../recoil/Quantity';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { patchOrderQuantity } from '../../api/PatchApi';
+import { OrderListAtom, QuantityListAtom } from '../../recoil/CartItem';
 
 interface QuantityType {
   idx: number;
@@ -9,23 +10,38 @@ interface QuantityType {
 }
 
 const QuantityInput = ({ idx, id }: QuantityType) => {
-  const [quantityList, setQuantityList] =
-    useRecoilState<{ id: string; quantity: number }[]>(QuantityListAtom);
+  const [quantityList, setQuantityList] = useRecoilState(QuantityListAtom);
+  const orderList = useRecoilValue(OrderListAtom);
 
+  /** 클릭한 input의 order아이디를 검색 */
+  const orderIdHandler = () => {
+    const orderId = orderList.filter(v => {
+      v.book.id === id;
+    })[0].id;
+
+    return orderId;
+  };
+
+  /** 빼기 */
   const Minus = (num: number) => {
     if (num > 1) {
       num -= 1;
       QuantityHandler(num);
+      patchOrderQuantity(orderIdHandler(), num);
     } else {
       QuantityHandler(1);
+      patchOrderQuantity(orderIdHandler(), 1);
     }
   };
 
+  /** 더하기 */
   const Plus = (num: number) => {
     num++;
     QuantityHandler(num);
+    patchOrderQuantity(orderIdHandler(), num);
   };
 
+  /** 입력한 숫자에 따라 quantityList 수정 */
   const QuantityHandler = (value: number) => {
     const copy = [...quantityList];
 
