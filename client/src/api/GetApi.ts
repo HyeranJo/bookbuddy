d0e2a4216a32de3f9a9af2ca1a199c0fdd9214f1
Dohyun Lee <120698922+AngryDoggaebi@users.noreply.github.com>
2023-09-01 13:28:43 +0900

    Feat fe/list (#20)
    * Feat: data 형식 수정 및 카테고리 기능 구현

--- client/src/api/GetApi.ts
+++ client/src/api/GetApi.ts
@@ -8,25 +8,25 @@ const SERVER_HOST = process.env.REACT_APP_SERVER_HOST;
 interface getBookListType {
   setListData: (result: BookList[]) => void;
   setIsLoading: (isloading: boolean) => void;
-  sidebarIdAtom: number;
+  sidebarId: number;
   page: number;
 }
 
 export const getBookList = async ({
   setListData,
   setIsLoading,
-  sidebarIdAtom,
+  sidebarId,
   page,
 }: getBookListType) => {
   setIsLoading(true);
   try {
     const response = await axios.get(
       // 카테고리 구현 완료시 사용
-      // `${SERVER_HOST}/book/list?page=${page}&size=20&category=${sidebarIdAtom}`,
-      `${SERVER_HOST}/book/list?page=${page}&size=20`,
+      `${SERVER_HOST}/book/list/${sidebarId}?page=${page}&size=20`,
+      // `${SERVER_HOST}/book/list?page=${page}&size=20`,
       { headers: { 'ngrok-skip-browser-warning': true } },
     );
-    const result = response.data;
+    const result = response.data.data;
     setListData(result);
     setIsLoading(false);
   } catch (error) {
