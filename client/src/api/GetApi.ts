d7e54c808e60b6c48eaa7746367e7c527670245b
AngryDoggaebi <aofhoa@gmail.com>
2023-09-01 13:23:12 +0900

    Feat: 타이틀 카테고리에 맞게 출력

--- client/src/api/GetApi.ts
+++ client/src/api/GetApi.ts
@@ -8,21 +8,21 @@ const SERVER_HOST = process.env.REACT_APP_SERVER_HOST;
 interface getBookListType {
   setListData: (result: BookList[]) => void;
   setIsLoading: (isloading: boolean) => void;
-  sidebarIdAtom: number;
+  sidebarId: number;
   page: number;
 }
 
 export const getBookList = async ({
   setListData,
   setIsLoading,
-  sidebarIdAtom,
+  sidebarId,
   page,
 }: getBookListType) => {
   setIsLoading(true);
   try {
     const response = await axios.get(
       // 카테고리 구현 완료시 사용
-      `${SERVER_HOST}/book/list/${sidebarIdAtom}?page=${page}&size=20`,
+      `${SERVER_HOST}/book/list/${sidebarId}?page=${page}&size=20`,
       // `${SERVER_HOST}/book/list?page=${page}&size=20`,
       { headers: { 'ngrok-skip-browser-warning': true } },
     );
