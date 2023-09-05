d44fdeb917bf9eab626f2eb6fce82261723f8af7
root <chldusehd789@gmail.com>
2023-09-05 13:06:41 +0900

    fix: 코드수정

--- client/src/components/book/Book.tsx
+++ client/src/components/book/Book.tsx
@@ -18,45 +18,23 @@ const Book = (props: BookProps) => {
   const navigate = useNavigate();
   const [isClick, setIsClick] = useState(false);
 
-  function ClickBookmark() {
-    setIsClick(isClick => !isClick);
-  }
-
   const postBookMark = async (id: string | undefined) => {
     try {
-      if (isClick === false) {
-        const response = await axios.post(
-          `${SERVER_HOST}/bookmark/${id}`,
-          {},
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: getCookie('accessToken'),
-            },
-          },
-        );
-        const result = response.data;
-        return result;
-      }
-    } catch (error) {
-      alert('error');
-    }
-  };
-
-  const deleteBookMark = async (id: string | undefined) => {
-    try {
-      if (isClick === true) {
-        const response = await axios.delete(`${SERVER_HOST}/bookmark/${id}`, {
+      const response = await axios.post(
+        `${SERVER_HOST}/bookmark/${id}`,
+        {},
+        {
           headers: {
             'Content-Type': 'application/json',
             Authorization: getCookie('accessToken'),
           },
-        });
-        const result = response.data;
-        return result;
-      }
+        },
+      );
+      const result = response.data;
+      setIsClick(result);
+      return result;
     } catch (error) {
-      alert(error);
+      alert('error');
     }
   };
 
@@ -72,10 +50,7 @@ const Book = (props: BookProps) => {
       </Styled_Book.wrapper>
       <Styled_Book.icon
         onClick={() => {
-          ClickBookmark;
           postBookMark(props.id);
-          deleteBookMark(props.id);
-          console.log(isClick);
         }}
       >
         <Bookmark
