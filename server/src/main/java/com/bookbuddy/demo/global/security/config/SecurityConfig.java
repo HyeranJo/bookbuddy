5d6a527352320fc577c85863e96a491d4d741656
johyeran <a01026823164@gmail.com>
2023-09-05 16:05:21 +0900

    Feat: 로그인 후 장바구니에 접근할 수 있도록 수정

--- server/src/main/java/com/bookbuddy/demo/global/security/config/SecurityConfig.java
+++ server/src/main/java/com/bookbuddy/demo/global/security/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/signup").permitAll()
                 .antMatchers("/signin").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/order/**").hasRole("USER")
 //                .antMatchers(HttpMethod.GET,"/bookmark/**").hasRole("USER")
 //                .antMatchers(HttpMethod.POST,"/bookmark/**").hasRole("USER")
                 .anyRequest().permitAll()
