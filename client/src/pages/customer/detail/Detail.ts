411c52e35c0f612f50d1ffb82daa0adbc4a8f352
AngryDoggaebi <aofhoa@gmail.com>
2023-09-09 20:29:42 +0900

    Feat: customer detail 파일 추가

---
+++ client/src/pages/customer/detail/Detail.ts
@@ -0,0 +1,5 @@
+import { styled } from 'styled-components';
+import { Styled_Layout } from '../../BlankPageLayout';
+import { DeviceQuery, screenScale } from '../../../utils/Responsive';
+
+const Styled_Detail = {};
