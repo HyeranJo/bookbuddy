bb25427b98e790f6f9c71b9aa1e05cadd86d4343
johyeran <a01026823164@gmail.com>
2023-09-11 14:28:45 +0900

    Feat: board, bookmark, payment에 트랜잭션 적용

--- server/src/main/java/com/bookbuddy/demo/payment/service/PaymentService.java
+++ server/src/main/java/com/bookbuddy/demo/payment/service/PaymentService.java
@@ -10,15 +10,20 @@ import com.bookbuddy.demo.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
+
+@Transactional(readOnly = true)
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final OrderService orderService;
+
+    @Transactional
     public Payment createPayment(Payment payment, PaymentDto.Post paymentDto) {
         paymentDto.getOrders().stream().map(e->{
             Order order = orderService.findOrder(e);
