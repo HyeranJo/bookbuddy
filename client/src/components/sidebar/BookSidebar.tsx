22e7be45a39ea478a9f49dff7212e5e4d3be8feb
AngryDoggaebi <aofhoa@gmail.com>
2023-09-15 21:21:29 +0900

    Feat: 페이지 첫 진입시 첫 번째 사이드바 메뉴에 포커스

--- client/src/components/sidebar/BookSidebar.tsx
+++ client/src/components/sidebar/BookSidebar.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Styled_Sidebar } from './BookSidebar.style';
 import { SidebarIdAtom } from '../../recoil/Sidebars';
 import { useSetRecoilState } from 'recoil';
 import category from '../../utils/SidebarCategory';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const BookSidebar = () => {
   const categoryKeys = Object.keys(category);
   const categoryValues = Object.values(category);
   const setSidebarId = useSetRecoilState(SidebarIdAtom);
   const navigate = useNavigate();
+  const firstCategoryRef = useRef<HTMLButtonElement>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    // 페이지 첫 진입시 사이드바 첫 번째 메뉴에 focus
+    if (firstCategoryRef.current && location.pathname === '/list') {
+      firstCategoryRef.current.focus();
+    }
+  }, []);
 
   /** 클릭한 사이드 메뉴 id를 recoil에 저장하는 함수 */
   const onClickHandler = (value: number) => {
@@ -20,17 +29,32 @@ const BookSidebar = () => {
     <Styled_Sidebar.Container>
       <Styled_Sidebar.Div>
         {categoryKeys.map((v, i) => {
-          return (
-            <Styled_Sidebar.Button
-              key={i}
-              onClick={() => {
-                onClickHandler(categoryValues[i]);
-                navigate('/list');
-              }}
-            >
-              {v}
-            </Styled_Sidebar.Button>
-          );
+          if (i === 0) {
+            return (
+              <Styled_Sidebar.Button
+                key={i}
+                onClick={() => {
+                  onClickHandler(categoryValues[i]);
+                  navigate('/list');
+                }}
+                ref={firstCategoryRef}
+              >
+                {v}
+              </Styled_Sidebar.Button>
+            );
+          } else {
+            return (
+              <Styled_Sidebar.Button
+                key={i}
+                onClick={() => {
+                  onClickHandler(categoryValues[i]);
+                  navigate('/list');
+                }}
+              >
+                {v}
+              </Styled_Sidebar.Button>
+            );
+          }
         })}
       </Styled_Sidebar.Div>
     </Styled_Sidebar.Container>
