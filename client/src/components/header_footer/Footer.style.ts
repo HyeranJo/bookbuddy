a23868cb09db153670b2f3af1687ff6df4a153d1
AngryDoggaebi <aofhoa@gmail.com>
2023-09-17 08:12:16 +0900

    Feat: footer 문자 추가

--- client/src/components/header_footer/Footer.style.ts
+++ client/src/components/header_footer/Footer.style.ts
@@ -17,6 +17,41 @@ export const Styled_Footer = {
     margin-top: 50px;
 
     z-index: 99;
-    background-color: wheat;
+    background-color: var(--footer-color);
+
+    & > div {
+      width: 1512px;
+      display: flex;
+      justify-content: end;
+      padding: 0 50px;
+
+      ${DeviceQuery.bigScreen`
+        width: calc(1512px / ${screenScale.bigScreen});
+        padding: 0 calc(50px / ${screenScale.bigScreen});
+      `}
+      ${DeviceQuery.desktop`
+        width: calc(1512px / ${screenScale.desktop});
+        padding: 0 calc(50px / ${screenScale.desktop});
+      `}
+      ${DeviceQuery.tablet`
+        width: calc(1512px / ${screenScale.tablet});
+        padding: 0 calc(50px / ${screenScale.tablet});
+      `}
+
+      & > span {
+        font-size: var(--basic-font-size);
+        color: var(--light-border-color);
+
+        ${DeviceQuery.bigScreen`
+          font-size: calc(var(--basic-font-size) / ${screenScale.bigScreen});
+        `}
+        ${DeviceQuery.desktop`
+          font-size: calc(var(--basic-font-size) / ${screenScale.desktop});
+        `}
+        ${DeviceQuery.tablet`
+          font-size: calc(var(--basic-font-size) / ${screenScale.tablet});
+        `}
+      }
+    }
   `,
 };
