e8dcdd41ff208d4dec20dcf3744b5e39e24ce012
Yeondong Choe <125066377+YeondongChoe@users.noreply.github.com>
2023-09-16 12:42:09 +0900

    Feat fe/fixsearch (#46)
    * fix: search

--- client/src/pages/customer/Customer.style.ts
+++ client/src/pages/customer/Customer.style.ts
@@ -4,7 +4,6 @@ import { DeviceQuery, screenScale } from '../../utils/Responsive';
 
 const Styled_Customer = {
   Main: styled(Styled_Layout.Container)`
-    /* flex-direction: column; */
     padding-left: 200px;
 
     ${DeviceQuery.bigScreen`
@@ -60,17 +59,17 @@ const Styled_Customer = {
   Warpper: styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    gap: 100px;
+    align-items: center;
+    gap: 150px;
 
     ${DeviceQuery.bigScreen`
-      gap: calc(100px / ${screenScale.bigScreen});
+      gap: calc(150px / ${screenScale.bigScreen});
     `}
     ${DeviceQuery.desktop`
-      gap: calc(100px / ${screenScale.desktop});
+      gap: calc(150px / ${screenScale.desktop});
     `}
     ${DeviceQuery.tablet`
-      gap: calc(100px / ${screenScale.tablet});
+      gap: calc(150px / ${screenScale.tablet});
     `}
   `,
 };
