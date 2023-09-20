f4a46c27fb6b9b8886b28f1f6a1511760504891e
AngryDoggaebi <aofhoa@gmail.com>
2023-09-20 14:06:39 +0900

    Fix: 주소입력 화면 api 주석 해제, 검열 추가

--- client/src/pages/payment/Payment.tsx
+++ client/src/pages/payment/Payment.tsx
@@ -74,30 +74,33 @@ const Payment = () => {
       alert('이메일 형식에 맞게 작성해주세요');
     } else if (
       allData.cstmrMobile.length !== 11 ||
-      (allData.cstmrTel && allData.cstmrTel.length < 9) ||
+      (allData.cstmrTel &&
+        allData.cstmrTel.length < 9 &&
+        allData.cstmrTel.length > 3) ||
       allData.shipMobile.length !== 11 ||
-      (allData.shipTel && allData.shipTel.length < 9)
+      (allData.shipTel &&
+        allData.shipTel.length < 9 &&
+        allData.shipTel.length > 3)
     ) {
       alert('전화번호 전체를 입력해주세요');
     } else {
-      console.log(allData);
       // api 전송
-      // let data = {
-      //   orderBooks: booksToPay.map(v => {
-      //     return { bookId: v.book.id, quantity: v.quantity };
-      //   }),
-      //   ...allDataCopy,
-      // };
+      let data = {
+        orderBooks: booksToPay.map(v => {
+          return { bookId: v.book.id, quantity: v.quantity };
+        }),
+        ...allDataCopy,
+      };
 
-      // postPaymentData(data) // api 전송
-      //   .then((data: any) => {
-      //     alert(`주문 완료되었습니다. 주문번호는 ${data.id} 입니다`);
-      //     booksToPay.map(v => DeleteCartItem(v.id));
-      //     navigate('/');
-      //   })
-      //   .catch(error => {
-      //     console.log('An error occurred:', error);
-      //   });
+      postPaymentData(data) // api 전송
+        .then((data: any) => {
+          alert(`주문 완료되었습니다. 주문번호는 ${data.id} 입니다`);
+          booksToPay.map(v => DeleteCartItem(v.id));
+          navigate('/');
+        })
+        .catch(error => {
+          console.log('An error occurred:', error);
+        });
     }
   };
 
