536344d1bd43d02692d4252eba15a84b0fa4b5f4
johyeran <a01026823164@gmail.com>
2023-09-23 00:07:14 +0900

    Feat: bookmark create test 단위 테스트로 변경 진행

--- server/src/test/java/com/bookbuddy/demo/bookmark/service/BookmarkServiceTest.java
+++ server/src/test/java/com/bookbuddy/demo/bookmark/service/BookmarkServiceTest.java
@@ -11,8 +11,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -20,39 +28,45 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @Transactional
 class BookmarkServiceTest {
-    @Autowired
+    @Mock
     private BookmarkRepository bookmarkRepository;
-    @Autowired
+    @Mock
     private MemberService memberService;
-    @Autowired
+    @Mock
     private BookService bookService;
-    @Autowired
+    @Mock
     private BookRepository bookRepository;
-    @Autowired
+    @InjectMocks
     private BookmarkService bookmarkService;
 
     @Test
     @DisplayName("북마크가 존재하지 않을 때 북마크생성")
     void 북마크가존재하지않을때북마크생성() {
         // given
-        memberService.createMember(new Member("test@test.com", "test1234!"));
-        Member findMember = memberService.findMember("test@test.com");
+        Member member = new Member("test@test.com", "test1234!");
+        Book book = new Book("test");
 
-        bookRepository.save(new Book("test"));
-        Book findBook = bookService.findBook("test");
+        given(bookmarkRepository.findByMemberAndBook(Mockito.any(Member.class), Mockito.any(Book.class)))
+                .willReturn(null);
+
+        given(bookmarkRepository.save(Mockito.any(Bookmark.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
 
         // when
-        Optional<Bookmark> findBookmark = bookmarkRepository.findByMemberAndBook(findMember, findBook);
+        Optional<Bookmark> findBookmark = Optional.ofNullable(bookmarkRepository.findByMemberAndBook(member, book))
+                .orElse(Optional.empty());
+
         Bookmark result = null;
         if(! findBookmark.isPresent()) {
             Bookmark createdBookmark = new Bookmark();
 
-            createdBookmark.addMember(findMember);
-            createdBookmark.addBook(findBook);
+            createdBookmark.addMember(member);
+            createdBookmark.addBook(book);
             result = bookmarkRepository.save(createdBookmark);
         }
 
